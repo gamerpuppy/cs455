254a4804820be5c79ca982c2bfae13636b06327d
gamerpuppy <keegan.millard@gmail.com>
2019-03-29 20:18:28 -0600

    asdf

---
+++ hw3/hadoop/setup-shared.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export HADOOP_CONF_DIR=/s/bach/e/under/millardk/cs455/hw3/hadoop/client-config/
---
+++ hw3/src/main/java/cs455/hadoop/Part1Mapper.java
@@ -0,0 +1,46 @@
+package cs455.hadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Mapper: Reads line by line, split them into words. Emit <word, 1> pairs.
+ */
+public class Part1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+
+
+        // tokenize into words.
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        // emit word, count pairs.
+        while (itr.hasMoreTokens()) {
+            String tok = itr.nextToken();
+            String res = evalToken(tok);
+
+            context.write(new Text(res), new IntWritable(1));
+        }
+    }
+
+    private static String evalToken(String tok) {
+        String res = "";
+        for(char c : tok.toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                res += Character.toLowerCase(c);
+            }
+        }
+
+        if(res.equals(""))
+            res = "!nonAlphabeticSequence";
+
+        return res;
+    }
+
+}
